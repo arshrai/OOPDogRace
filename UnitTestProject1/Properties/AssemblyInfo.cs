3d406fa311fbef7eee5082d3b284d4d2ee67a364
anshurai98 <anshurai98@gmail.com>
2019-12-06 09:40:50 +1300

    changes

--- UnitTestProject1/Properties/AssemblyInfo.cs
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -1,12 +1,11 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyTitle("DogRace_UnitTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HP Inc.")]
-[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyProduct("DogRace_UnitTest")]
 [assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
